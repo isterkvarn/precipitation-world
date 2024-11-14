29d30921af208d8d17242185a6e30e86508ffd86
D0V4HKIIN <jonas.bonnaudet@gmail.com>
2024-11-14 10:52:35 +0100

    updates position on each physics frame

--- marching_cubes/terrain_cpu.gd
+++ marching_cubes/terrain_cpu.gd
@@ -738,28 +738,30 @@ func show_chunk(coord: Vector3i) -> void:
 			var thread := get_worker_thread()
 			thread.start(march_chunk_gpu.bind(coord, duplicate_2d(TRIANGULATIONS)))
 
-func show_chunks_around_player(player_chunk: Vector3i) -> void:
+
+var player_chunk: Vector3i = Vector3i(0,0,0)
+func show_chunks_around_player() -> void:
 	#Gör så att vi utgår från player position och rör oss utåt.
 	var max_offset = floor(RENDER_DISTANCE/2)
 	for offset in range(0, max_offset + 1):
 		for dx in range(-offset, offset+1):
 			for dy in range(-offset, offset+1):
 				for dz in range(-offset, offset+1):
-					if abs(dx) == offset or abs(dy) == offset or abs(dz) == offset:
-						#var x = max_offset + dx 
-						#var y = max_offset + dy
-						#var z = max_offset + dz
-						#var chunk_coord := player_chunk + Vector3i(dx, dy, dz) - Vector3i(RENDER_DISTANCE / 2, RENDER_DISTANCE / 2, RENDER_DISTANCE / 2)
-						var chunk_coord := player_chunk + Vector3i(dx, dy, dz)
-						print("Player chunk: " , player_chunk , " Current chunk: " , chunk_coord , " x,y,z: " ,dx ,",", dy ,",", dz)
-						show_chunk(chunk_coord)
+					#if abs(dx) == offset or abs(dy) == offset or abs(dz) == offset:
+					#var x = max_offset + dx 
+					#var y = max_offset + dy
+					#var z = max_offset + dz
+					#var chunk_coord := player_chunk + Vector3i(dx, dy, dz) - Vector3i(RENDER_DISTANCE / 2, RENDER_DISTANCE / 2, RENDER_DISTANCE / 2)
+					var chunk_coord := player_chunk + Vector3i(dx, dy, dz)
+					print("Player chunk: " , player_chunk , " Current chunk: " , chunk_coord , " x,y,z: " ,dx ,",", dy ,",", dz)
+					show_chunk(chunk_coord)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	# big oopsie if there is more than 1 player
+	for player: Node3D in get_tree().get_nodes_in_group("player"):
+		player_chunk = Vector3i(floor(player.position.x / CHUNK_SIZE), floor(player.position.y / CHUNK_SIZE), floor(player.position.z / CHUNK_SIZE))
 	if (chunk_thread.is_started() && !chunk_thread.is_alive()):
 		chunk_thread.wait_to_finish()
-		# big oopsie if there is more than 1 player
-		for player: Node3D in get_tree().get_nodes_in_group("player"):
-			var player_chunk := Vector3i(floor(player.position.x / CHUNK_SIZE), floor(player.position.y / CHUNK_SIZE), floor(player.position.z / CHUNK_SIZE))
-			chunk_thread.start(show_chunks_around_player.bind(player_chunk))
+		chunk_thread.start(show_chunks_around_player)
